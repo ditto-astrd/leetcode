22c03a227e9bfb92e5048c96088f84415937643d
Ditto in Pangyo <astrid.prcss@gmail.com>
2024-10-24 18:36:10 +0900

    Time: 6 ms (46.59%), Space: 41.4 MB (74.4%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -6,12 +6,12 @@ class Solution {
         while(pos < target.length()) {
             int prevPos = pos;
             
-            /* source길이만큼 for문을 돌리고, source.charAt(i)와 target.charAt(pos)가 같으면 pos++ */
-            for(int i=0; i<source.length(); i++){
-                if(pos < target.length() && source.charAt(i) == target.charAt(pos)) pos++;
+            for (int i = 0; i < source.length(); i++) {
+                if (pos < target.length() && source.charAt(i) == target.charAt(pos)) pos++;
             }
+            
+            if (prevPos == pos) return -1;
 
-            if(prevPos == pos) return -1;
             ans++;
         }
 
@@ -19,7 +19,13 @@ class Solution {
     }
 }
 
+/**
+- subsquence = 원문 문자열에서 일부 문자를 제거해서 나온 문자
+  - 이때 상대적인 위치는 변경하면 안됨
+- source에서 target을 만들기 위한 subsequence의 최소 개수
+- 불가능하면 -1 return
 
-/*
-    1. 
+1. target 길이 만큼 while문을 돌림
+2. 내부에서 source길이만큼 for문을 돌리고, source.charAt(i)와 target.charAt(pos)가 같으면 pos++
+
  */
