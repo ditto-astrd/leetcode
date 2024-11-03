ef27797f8e0a41ff20c404ae0ec246343077d56f
Ditto in Pangyo <astrid.prcss@gmail.com>
2024-11-03 21:22:49 +0900

    Time: 1 ms (99.14%), Space: 44.4 MB (98.38%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,6 +1,24 @@
 class Solution {
     public int jump(int[] nums) {
         int len = nums.length;
+        int jump = 0;
+        int cur = 0, far = 0;
+
+        for (int i = 0; i < len - 1; i++) {
+            far = Math.max(far, i + nums[i]);
+
+            if (cur == i) {
+                jump++;
+                cur = far;
+            }
+        }
+
+        return jump;
+    }
+}
+/**
+    public int jump(int[] nums) {
+        int len = nums.length;
         int[] dp = new int[len];
 
         Arrays.fill(dp, Integer.MAX_VALUE);
@@ -15,4 +33,4 @@ class Solution {
 
         return dp[len - 1];
     }
-}
+ */
