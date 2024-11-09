01002dccf23848a97b7c73dec7925ff30e535fe7
Ditto in Pangyo <astrid.prcss@gmail.com>
2024-11-09 17:14:41 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0422-valid-word-square/0422-valid-word-square.java
@@ -0,0 +1,20 @@
+What is the time complexity and space complexity of the code below?
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        for(int i = 0; i < words.size(); i++) {
+            String temp1 = "";
+            String temp2 = "";
+            for(int j = 0; j < words.get(i).length(); j++){
+                Character c1 = words.get(i).charAt(j);
+                Character c2 = 'A';
+                if(words.size() >= j+1 && words.get(j).length() >= i+1) { 
+                    c2 = words.get(j).charAt(i);
+                }
+                temp1 += c1.toString();
+                temp2 += c2.toString();
+                if(!temp1.equals(temp2)) return false;
+            }
+        }
+        return true;
+    }
+}
