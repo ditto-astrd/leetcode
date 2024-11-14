09cfafbae2b37e8d24f0ab0e44203a057f243d54
Ditto in Pangyo <astrid.prcss@gmail.com>
2024-11-14 22:42:45 +0900

    Time: 12 ms (48.83%), Space: 44.3 MB (62.02%) - LeetHub

--- 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.java
+++ 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.java
@@ -1,26 +1,33 @@
 class Solution {
+    private static final String BOLD_START = "<b>";
+    private static final String BOLD_END = "</b>";
+
     public String addBoldTag(String s, String[] words) {
-        boolean[] check = new boolean[s.length()];
+        int len = s.length();
+        boolean[] check = new boolean[len];
         StringBuilder sb = new StringBuilder();
         
-        for(int i = 0, end = 0; i < s.length(); i++) {
+        for (int i = 0, end = 0; i < len; i++) {
             for(String word : words) {
-                if(s.startsWith(word, i)) {
+                if (s.startsWith(word, i)) {
                     end = Math.max(end, i + word.length());
                 }
             }
+
             check[i] = end > i;
         }
-        
-        for(int i = 0; i < s.length(); i++) {
-            if(check[i]) {
-                int j = i;
-                for(j = i; j < s.length(); j++) {
+
+        for (int i = 0; i < len; i++) {
+            if (check[i]) {
+                int j;
+                for (j=i; j < len; j++) {
                     if(!check[j]) break;
                 }
-                sb.append("<b>"+s.substring(i,j)+"</b>");
-                i=j-1;
-            } else sb.append(s.charAt(i));
+                sb.append(BOLD_START + s.substring(i, j) + BOLD_END);
+                i = j - 1;
+            } else {
+                sb.append(s.charAt(i));
+            }
         }
 
         return sb.toString();
@@ -28,6 +35,9 @@ class Solution {
 }
 
 /**
+- 만약 겹치는 words가 존재하면 하나의 태그로 합쳐야 한다.
+- 겹치지 않고 연이은 words가 존재하면 하나의 태그로 합쳐야 한다.
+
 1. startWith으로 단어의 시작 지점, 종료 지점을 판별
 2. 시작 / 종료지점을 true로 표기할때 이미 true가 된 지점이라면
     -> 앞에 단어랑 위치가 중복된다는 뜻
